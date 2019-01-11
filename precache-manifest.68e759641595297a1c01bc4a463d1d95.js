f0360d859e4d645e7eb04cedae5ac70c8bb56e75
therealmcp <cp.mcpike@gmail.com>
2019-01-10 22:31:52 -0800

    Updates

---
+++ precache-manifest.68e759641595297a1c01bc4a463d1d95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "772a52d5f093321c297a",
+    "url": "/ClickyGame/static/js/main.772a52d5.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/ClickyGame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "772a52d5f093321c297a",
+    "url": "/ClickyGame/static/css/main.82d86756.chunk.css"
+  },
+  {
+    "revision": "0c1b5886670a4e8386d95a65b11d48b2",
+    "url": "/ClickyGame/index.html"
+  }
+];
